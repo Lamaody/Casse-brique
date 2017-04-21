3c2f6171ab540ef9cdf5af054794c67d0dd99d62
Lamaody <raoelijaonaallan@gmail.com>
2017-04-21 22:29:38 +0300

    Update run.js

--- js/run.js
+++ js/run.js
@@ -2,10 +2,32 @@
 window.addEventListener('load', init);
 
 let canvas, theGame;
+let couleurBalleDepart, couleurFond, couleurBarre, cafe, lose;
 
 function init() {
   canvas = document.querySelector("#myCanvas");
-  theGame = new MoteurJeu();
-  theGame.start();
+  cafe = document.getElementById("cafe");
+  lose = document.getElementById("lose");
+}
+
+function validerReglage(){
+	
+   couleurBalleDepart = document.getElementById("couleurBalleDepart").value;
+   couleurFond = document.getElementById("couleurFond").value;
+   couleurBarre = document.getElementById("couleurBarre").value;
+   document.getElementById("myCanvas").style.background = couleurFond;
+   
+   theGame = new MoteurJeu();
+}
+
+function commencerLeJeu(){
+	
+	document.getElementById("couleurFond").disabled=true;
+	document.getElementById("couleurBalleDepart").disabled=true;
+	document.getElementById("couleurBarre").disabled=true;
+	
+	document.getElementById("valider").style.display="none";
+	document.getElementById("commencer").style.display="none";
+	theGame.start();
 }
 
